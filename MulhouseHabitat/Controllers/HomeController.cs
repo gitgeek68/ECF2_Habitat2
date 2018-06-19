caa594da6387e230b7ea62752fc81466b29d5dfb
Mhamidi <hamidi.michael@outlook.fr>
2018-06-19 16:45:02 +0200

    17h cata

---
+++ MulhouseHabitat/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MulhouseHabitat.Models;
+using System.IO;
+
+
+namespace MulhouseHabitat.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MulhouseHabitat/Controllers/LogementController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MulhouseHabitat.Models;
+using System.IO;
+
+namespace MulhouseHabitat.Controllers
+{
+    public class LogementController : Controller
+    {
+
+        Dal dal;
+
+        public LogementController()
+        {
+            dal = new Dal();
+        }
+
+        public ActionResult Index()
+        {
+            return View(dal.GetLogements());
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(MHLogements logements)
+        {
+            bool isCorrect = false;
+            for (int i = 0; i < GetLogements.Count; i++)
+            {
+                if (logements.Id == dal.GetLogements[i].Count)//egale au predicat en dessous
+                {
+                    isCorrect = true;
+
+                }
+
+            }
+            if (isCorrect == false)
+            {
+                //Produits.Add(p);
+                dal.GetLogements(logements);
+
+            }
+            return RedirectToAction("Index");//redirection vers la vue index
+        }
+
+        public ActionResult Detail (string id)
+        {
+            MHLogements l = dal.GetLogement(id);
+        }
+    }
+}

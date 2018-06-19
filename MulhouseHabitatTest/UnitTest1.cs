c3d35aac5bb2e2a987c9b477670d1039bcf59ea5
Mhamidi <hamidi.michael@outlook.fr>
2018-06-19 14:50:58 +0200

    partie1
    test reussi

--- MulhouseHabitatTest/UnitTest1.cs
+++ MulhouseHabitatTest/UnitTest1.cs
@@ -7,12 +7,30 @@ namespace MulhouseHabitatTest
     [TestClass]
     public class UnitTest1
     {
+
+        Dal dal = new Dal();
+
+
         [TestMethod]
         public void TestMethod1()
         {
+            MHLogements logements = new MHLogements();
+
+            logements.Type = "appartement";
+            logements.StreetNumber = "9";
+            logements.StreetName = "Jean des Fontaine";
+            logements.PostalCode = "31400";
+            logements.City = "Toulouse";
+            logements.NumberOfPieces = 5;
+            logements.Size = 75;
+            logements.Rented = false;
 
 
+            int nbLogements = dal.GetLogements().Count;
 
+            dal.AddLogement(logements);
+
+            Assert.AreEqual((nbLogements + 1), dal.GetLogements().Count);
         }
     }
 }
